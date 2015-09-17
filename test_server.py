110a88f8a278b84f09a18c5060b3dbdf25708fff
Jakub Krajniak <jkrajniak@gmail.com>
2015-09-17 09:39:26 +0200

    first version with socketio

---
+++ test_server.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+#
+# Copyright (c) 2015 Jakub Krajniak <jkrajniak@gmail.com>
+#
+# Distributed under terms of the GNU GPLv3 license.
+#
+
+from flask import Flask, render_template, Response, request
+#from camera import Camera
+import cv2
+import Image
+import StringIO
+from flask.ext.socketio import SocketIO, emit
+import base64
+from gevent.wsgi import WSGIServer
+
+
+class Camera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, png = cv2.imencode('.png', image)
+        return png.tostring()
+
+
+app = Flask(__name__)
+app.debug = False
+socketio = SocketIO(app)
+
+
+def gen(cam):
+    while True:
+        frame = cam.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/processing')
+def processing():
+    return render_template('processing.html')
+
+
+@socketio.on('connect')
+def on_connect(message):
+    print(">>>>{}".format(message))
+
+
+@app.route('/update_log/', methods=['GET'])
+def update_log():
+    source = request.args['source']
+    status = request.args['status']
+    message = request.args['message']
+    print('Emmit from {} message "{}"'.format(source, message))
+    socketio.emit('update log', {'source': source, 'message': message, 'status': status})
+    return Response(message)
+
+if __name__ == '__main__':
+    from socketio.server import SocketIOServer
+    SocketIOServer(('0.0.0.0', 8080), app,
+                   resource="socket.io", policy_server=False).serve_forever()
