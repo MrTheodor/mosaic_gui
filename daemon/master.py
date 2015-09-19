64c302509dda5b83c424f182115e8bafe2bafe7c
Jakub Krajniak <jkrajniak@gmail.com>
2015-09-19 15:16:39 +0200

    sending email

--- daemon/master.py
+++ daemon/master.py
@@ -8,14 +8,22 @@
 from mpi4py import MPI
 import os
 import shutil
+import smtplib
 import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.utils import formatdate
 
 import plogger
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-SNAPSHOT = "/home/teodor/project/mosaic_web/daemon/files/snapshot.jpg"
+# Load params from file.
+SNAPSHOT = None
+SMTP_SERVER = None
+SEND_FROM = None
+execfile('params.par')
 
 logger = plogger.PLogger(rank)
 
@@ -29,12 +37,32 @@ def process(data):
     logger.write('result send {}'.format(data['email']))
     if os.path.exists(SNAPSHOT):
         logger.write('found {}'.format(SNAPSHOT))
+
     # Wait for other nodes to finish
     comm.Barrier()
 
     # Dummy, only for test, copy input file to output_<timestamp>.jpg
+    # and send e-mail.
     basedir = os.path.dirname(SNAPSHOT)
     output_file = 'output_{}.jpg'.format(time.time())
     print('Copy {} -> {}'.format(SNAPSHOT, os.path.join(basedir, output_file)))
     shutil.copyfile(SNAPSHOT, os.path.join(basedir, output_file))
+    print('Send email to {}'.format(data['email']))
+    msg = MIMEMultipart(
+        From=SEND_FROM,
+        To=data['email'],
+        Date=formatdate(localtime=True),
+        Subject='opendag superpi mosaic'
+        )
+    with open(os.path.join(basedir, output_file), 'rb') as output_mosaic:
+        msg.attach(
+            MIMEApplication(
+                output_mosaic.read(),
+                Content_Disposition='attachment; filename="{}"'.format(output_file),
+                Name=output_file
+            ))
+    smtp = smtplib.SMTP(SMTP_SERVER)
+    smtp.sendmail(SEND_FROM, data['email'], msg.as_string())
+    smtp.close()
     logger.emit_finished(output_file)
+    logger.write('finished', status=plogger.FINISHED)
--- daemon/plogger.py
+++ daemon/plogger.py
@@ -7,17 +7,28 @@
 
 import urllib
 
+IDLE = 0
+DOWNLOAD = 1
+FINISHED = 2
+MATCHING = 3
+
+valid_status = [IDLE, DOWNLOAD, FINISHED, MATCHING]
+
 
 class PLogger(object):
     def __init__(self, source_rank, initial_status=0, host_url='http://0.0.0.0:5050'):
         self.rank = source_rank
         self.status = 0
+        if self.status not in valid_status:
+            raise Exception('Invalid status')
         self.update_url = '{}/update_log/?'.format(host_url)
         self.host_url = host_url
 
     def write(self, message, status=None):
         if status is not None:
             self.status = status
+        if self.status not in valid_status:
+            raise Exception('Invalid status {}'.format(status))
         params = {'source': self.rank, 'message': message, 'status': self.status}
         update_url = self.update_url + urllib.urlencode(params)
         try:
